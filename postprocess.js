8e5a454bbe14482280c26b9921d3d7ebc3e3b520
anpigon <markan82@gmail.com>
2021-07-08 08:49:26 +0900

    test

--- postprocess.js
+++ postprocess.js
@@ -1,12 +1,29 @@
-import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts'
+import { readJSON, writeJSON } from "https://deno.land/x/flat/mod.ts";
 
-const filename = Deno.args[0] 
-const data = await readJSON(filename)
-const newData = data.result.filter(item => {
-  return (["USD-BTC", "USD-USDT", "USDT-STEEM", "BTC-STEEM", "BTC-SBD"].includes(item.MarketName));
-})
+/// bittrex
+async function bittrex() {
+  const filename = Deno.args[0];
+  const data = await readJSON(filename);
+  const newData = data.result.filter((item) => {
+    return ["USD-BTC", "USD-USDT", "USDT-STEEM", "BTC-STEEM", "BTC-SBD"].includes(
+      item.MarketName
+    );
+  });
+  await writeJSON("flat/bittrex_price.json", newData);
+}
+await bittrex();
 
-const newfile = 'bittrex_price.json'
-await writeJSON(newfile, newData)
-
-// https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=STEEM,SBD
+/// coinmarketcap
+async function coinmarketcap() {
+  const response = await fetch(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=STEEM,SBD,TRX,BTC,USDT",
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+      },
+    }
+  );
+  const { data } = await response.json();
+  await writeJSON("flat/coinmarketcap_price.json", data);
+}
+await coinmarketcap();
