738936a0c0c466ea15a29122c3d1e501a18410a2
anpigon <markan82@gmail.com>
2021-07-08 08:26:56 +0900

    first commit

---
+++ postprocess.js
@@ -0,0 +1,12 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts'
+
+const filename = Deno.args[0] 
+const data = await readJSON(filename)
+const newData = data.result.filter(item => {
+  return (["USD-BTC", "USD-USDT", "USDT-STEEM", "BTC-STEEM", "BTC-SBD"].includes(item.MarketName));
+})
+
+const newfile = 'price.json'
+await writeJSON(newfile, newData)
+
+// https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=STEEM,SBD
